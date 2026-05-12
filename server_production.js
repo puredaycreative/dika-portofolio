b5241f7ba5c349cfe15890e2fb2d3c1390f80549
puredaycreative <puredaycreative@gmail.com>
2026-05-12 22:29:52 +0700

    first commit

--- server_production.js
+++ server_production.js
@@ -2,30 +2,21 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import cors from 'cors';
-import fs from 'fs';
 
 const app = express();
-
-// Konfigurasi path untuk ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// CORS
-app.use(cors({
-  origin: [
-    "https://dika-portofolio-ashy.vercel.app",
-  ]
-}));
-
+app.use(cors());
 app.use(express.json());
 
-// Sajikan file statis (agar CSS/JS di root terbaca)
+// Sajikan file statis (CSS, JS frontend, dll)
 app.use(express.static(__dirname));
 
-// Rute utama untuk menjalankan index.html
+// Rute utama
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Export app untuk Vercel (Tanpa app.listen)
+// PENTING: Untuk Vercel, jangan pakai app.listen()
 export default app;
