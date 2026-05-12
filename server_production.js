5c13f4152e01cec02e677f3a26c2177a0a35da0f
puredaycreative <puredaycreative@gmail.com>
2026-05-12 22:38:25 +0700

    first commit

--- server_production.js
+++ server_production.js
@@ -10,14 +10,18 @@ const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(express.json());
 
+// 1. Prioritaskan folder khusus agar tidak tertutup rute lain
 app.use('/css', express.static(path.join(__dirname, 'css')));
 app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/images', express.static(path.join(__dirname, 'images'))); // Jika ada folder gambar
 
-// 2. Sajikan folder root untuk file statis lainnya (gambar, js, dll)
+// 2. Sajikan file statis umum dari root (seperti favicon.ico atau manifest)
 app.use(express.static(__dirname));
 
+// 3. Rute utama paling bawah
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Export untuk Vercel
 export default app;
